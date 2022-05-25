25edc68e566e61ff433ea5a571c251d45df89f00
helenfromboston <helenfromboston@gmail.com>
2022-05-25 12:51:03 +0300

    Fixed task 060

--- Task060_3DArrayUnique/Program.cs
+++ Task060_3DArrayUnique/Program.cs
@@ -1,19 +1,9 @@
 ﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
-void Fill3DArray(int[,,] array)
-{
-    for (int x = 0; x < array.GetLength(0); x++)
-    {
-        for (int y = 0; y < array.GetLength(1); y++)
-        {
-            for (int z = 0; z < array.GetLength(2); z++)
-            {
-                array[x, y, z] = new Random().Next(10, 100);
-            }
-        }
-    }
-}
+// Комментарий по решению: в условии не сказано, должен ли быть массив задан случайными числами.
+// Поэтому в решении массив последовательно заполняется от 10 до 99.
+// Заполнить неповторяющимися числами с использованием рандома у меня не получилось.
 
 void Print3D(int[,,] array)
 {
@@ -33,8 +23,24 @@ void Print3D(int[,,] array)
 }
 
 int[,,] mass = new int[3, 4, 5];
+int value = 10;
 
-Fill3DArray(mass);
-Print3D(mass);
-
-// НЕ РЕШЕНО
+if (mass.Length>(99-10))
+{
+    Console.WriteLine("Трёхмерный массив заданного размера заполнить неповторяющимися двузначными числами невозможно");
+}
+else
+{
+    for (int x = 0; x < mass.GetLength(0); x++)
+    {
+        for (int y = 0; y < mass.GetLength(1); y++)
+        {
+            for (int z = 0; z < mass.GetLength(2); z++)
+            {
+                mass[x, y, z] = value;
+                value++;
+            }
+        }
+    }
+    Print3D(mass);
+}
